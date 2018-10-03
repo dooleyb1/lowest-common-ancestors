3a3f6329fb66cb55ffc28ee1be39b236f67f1098
dooleyb1 <dooleyb1@tcd.ie>
2018-10-03 11:08:13 +0100

    added first test case

--- dag/javascript/test.js
+++
@@ -1,1 +0,0 @@
-var findAncestor = require('./index')
---
+++ dag/javascript/test/test.js
@@ -0,0 +1,28 @@
+var findAncestor = require('../index');
+var assert = require('assert');
+
+describe('Basic DAG LCA Test', function () {
+  // First create DAG
+  var nodes = {
+    1: [],
+    2: [1],
+    3: [2],
+    4: [2],
+    5: [4],
+    6: [3, 5],
+    7: [6],
+    8: [5],
+    9: [8]
+  }
+
+  var readParents = function(id, cb) {
+    cb(null, nodes[id])
+  }
+
+  // LCA Test for nodes [9,7] in DAG
+  it('Should return the LCA of nodes [9,7] as node 5', function () {
+    findAncestor([9,7], readParents, function(err, res) {
+      assert.equal(res, 5);
+    })
+  });
+});
